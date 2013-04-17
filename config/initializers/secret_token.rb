c19b112278f65f3b5eecb5eaec339bf4abf24b4e
inos <ocean@snu.ac.kr>
2013-04-17 20:04:30 +0900

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Likelion417::Application.config.secret_token = 'e2ee6d4b752845481fbe307dfab3560bc177c38ef8e03b5316932099b54fdbe23220b92d74f2f1a134aab6714850062000d197a3934b1c4d86469d0ce44f2937'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Likelion417::Application.config.session_store :cookie_store, key: '_likelion417_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Likelion417::Application.config.session_store :active_record_store
